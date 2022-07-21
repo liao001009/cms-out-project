f017a13d6530ac2c108bde75696a1e0c2fd8cf53
王鼎 <wangd_nc@landray.com.cn>
2022-07-21 09:40:31 +0800

    fix: #000 导出项目需求模块

--- src/desktop/api.ts
+++ src/desktop/api.ts
@@ -5,5 +5,19 @@ import CmsProjectInfoEdit from '@/desktop/pages/cmsProjectInfo/edit/content'
 import CmsProjectInfoList from '@/desktop/pages/cmsProjectInfo/list-ProjectInfo/content'
 import CmsProjectInfoView from '@/desktop/pages/cmsProjectInfo/view/content'
 
+import CmsProjectWrittenAdd from '@/desktop/pages/cmsProjectWritten/add/content'
+import CmsProjectWrittenEdit from '@/desktop/pages/cmsProjectWritten/edit/content'
+import CmsProjectWrittenView from '@/desktop/pages/cmsProjectWritten/view/content'
+import CmsProjectWrittenList from '@/desktop/pages/cmsProjectWritten/list-Written/content'
 
-export { CmsProjectInfoAdd, CmsProjectInfoEdit, CmsProjectInfoList, CmsProjectInfoView }
+
+export {
+  CmsProjectInfoAdd,
+  CmsProjectInfoEdit,
+  CmsProjectInfoList,
+  CmsProjectInfoView,
+  CmsProjectWrittenAdd,
+  CmsProjectWrittenEdit,
+  CmsProjectWrittenView,
+  CmsProjectWrittenList
+}
